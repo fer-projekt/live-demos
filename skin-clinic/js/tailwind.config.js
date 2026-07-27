24f1d60a84f1dddeba4035318b1793f84e1c895d
LukaFerProjekt <luka@fer-projekt.com>
2026-07-27 12:31:41 +0200

    updated skin-clinic

---
+++ skin-clinic/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          sand:  '#E0CEBD',
+          taupe: '#C0A392',
+          cream: '#F6F0EA',
+          ink:   '#39322D',
+          mute:  '#8C7F74',
+        },
+        fontFamily: {
+          serif: ['"Cormorant Garamond"', 'Georgia', 'serif'],
+          sans:  ['Jost', 'system-ui', 'sans-serif'],
+        },
+      },
+    },
+  };
